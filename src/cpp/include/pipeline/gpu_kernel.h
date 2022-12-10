0f21777144287786a62c72bd0151b51ab3deeef2
Rahul Chunduru <rahulchunduru@Rahuls-MacBook-Pro.local>
2022-12-10 14:42:00 -0600

    fix errors attempt 2

--- src/cpp/include/pipeline/gpu_kernel.h
+++ src/cpp/include/pipeline/gpu_kernel.h
@@ -2,6 +2,6 @@
 // Created by Rahul Chunduru on 12/22.
 //
 
-void launchKernel(void* pipeline_, int gpu_id_, void* batch);
+// void launchKernel(void* pipeline_, int gpu_id_, void* batch);
 
-void transferDataToDevice(void* pipeline_, void* batch, int queue_choice)
+void transferDataToDevice(void* pipeline_, void* batch, int queue_choice);
